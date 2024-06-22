7fab523143ca9673fc276eae56939c940ade9cda
Orovwode Great <orovwodegreat@gmail.com>
2024-06-22 16:56:03 +0100

    another commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var tally= 0;
+// while (tally < 20){
+//     console.log( tally);
+//     tally+=2
+// }
+
+var text="hello";
+while (tally < text.length){
+    console.log(text[tally])
+    tally+=1
+}
+// str.length
